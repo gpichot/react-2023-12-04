07b76e847a713ea1dd8e3b68979db2ac3485ec2c
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-12-05 11:57:52 +0100

    feat: add day 2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import { ProductItem } from "./components/ProductItem";
 
@@ -9,12 +10,25 @@ const products = [
 ];
 
 function App() {
+  const [searchText, setSearchText] = React.useState("");
+
+  const filteredProducts = products.filter((product) => {
+    return product.name.toLowerCase().includes(searchText.toLowerCase());
+  });
+
+  const handleSearchTextChange = (
+    event: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    setSearchText(event.currentTarget.value);
+  };
   return (
     <div>
       <h1>My Bakery</h1>
       <p>Let's shop some bread and pastries</p>
+      <input type="text" value={searchText} onChange={handleSearchTextChange} />
+
       <ul>
-        {products.map((product) => {
+        {filteredProducts.map((product) => {
           return (
             <ProductItem key={product.id} product={product}>
               <strong>{product.description}</strong>
--- my-app/src/components/ProductItem.tsx
+++ my-app/src/components/ProductItem.tsx
@@ -11,22 +11,36 @@ type ProductItemProps = {
 export function ProductItem(props: ProductItemProps) {
   const { product, children } = props;
   const { name } = product;
+  const [isHovered, setIsHovered] = React.useState(false);
+  const [quantity, setQuantity] = React.useState(0);
 
-  const handleMouseEnter = (e: React.MouseEvent<HTMLElement>) => {
-    console.log(`enter ${product.name}`, e);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
   };
-  const handleMouseLeave: React.MouseEventHandler<HTMLElement> = (e) => {
-    console.log(`leave ${product.name}`, e);
+  const handleMouseLeave: React.MouseEventHandler<HTMLElement> = () => {
+    setIsHovered(false);
+  };
+
+  const handleIncrement = () => {
+    setQuantity(quantity + 1);
+  };
+  const handleDecrement = () => {
+    setQuantity(quantity - 1);
   };
   return (
     <li
-      style={{ backgroundColor: "purple" }}
+      style={{ backgroundColor: isHovered ? "#88888811" : "#11111122" }}
       onClick={() => console.log(product)}
       onMouseEnter={handleMouseEnter}
       onMouseLeave={handleMouseLeave}
     >
       {name}
       {children}
+      <div>
+        <button onClick={handleIncrement}>+</button>
+        {quantity}
+        <button onClick={handleDecrement}>-</button>
+      </div>
     </li>
   );
 }
