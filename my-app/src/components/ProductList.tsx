e92e624d0bb1691bb38a725937a55c80b88d431f
Gabriel Pichot <gabriel.pichot@gmail.com>
2023-12-06 12:42:56 +0100

    feat: add product list query with pagination

--- my-app/src/components/ProductList.tsx
+++ my-app/src/components/ProductList.tsx
@@ -3,34 +3,19 @@ import { ProductItem } from "./ProductItem";
 import { useProductListQuery } from "../queries";
 
 export function ProductList() {
-  const [searchText, setSearchText] = React.useState("");
-
-  const handleSearchTextChange = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    setSearchText(event.currentTarget.value);
-  };
-
-  React.useEffect(() => {
-    document.title = `Searching for ${searchText}`;
-  }, [searchText]);
-
   return (
     <div>
       <h1>My Bakery</h1>
       <p>Let's shop some bread and pastries</p>
-      <input type="text" value={searchText} onChange={handleSearchTextChange} />
-      <ProductListResults searchText={searchText} />
+
+      <ProductListResults />
     </div>
   );
 }
 
-interface ProductListResultsProps {
-  searchText: string;
-}
-
-function ProductListResults({ searchText }: ProductListResultsProps) {
-  const productsQuery = useProductListQuery();
+function ProductListResults() {
+  const [page, setPage] = React.useState(1);
+  const productsQuery = useProductListQuery({ page });
 
   if (productsQuery.isLoading) {
     return <p>Loading</p>;
@@ -46,18 +31,27 @@ function ProductListResults({ searchText }: ProductListResultsProps) {
     );
   }
 
-  const filteredProducts = products.filter((product) => {
-    return product.name.toLowerCase().includes(searchText.toLowerCase());
-  });
   return (
-    <ul>
-      {filteredProducts.map((product) => {
-        return (
-          <ProductItem key={product.id} product={product}>
-            <strong>{product.description}</strong>
-          </ProductItem>
-        );
-      })}
-    </ul>
+    <>
+      <button
+        onClick={() => setPage((currentPage) => Math.max(1, currentPage - 1))}
+      >
+        Previous
+      </button>
+      Page {page}
+      <button onClick={() => setPage((currentPage) => currentPage + 1)}>
+        Next
+      </button>
+      {productsQuery.isFetching && "Fetching new data"}
+      <ul>
+        {products.map((product) => {
+          return (
+            <ProductItem key={product.id} product={product}>
+              <strong>{product.description}</strong>
+            </ProductItem>
+          );
+        })}
+      </ul>
+    </>
   );
 }
--- my-app/src/queries.ts
+++ my-app/src/queries.ts
@@ -1,17 +1,23 @@
-import { useQuery } from "@tanstack/react-query";
+import { useQuery, keepPreviousData } from "@tanstack/react-query";
 import { Product } from "./types";
 
-async function fetchProducts({ page }) {
+const sleep = () => new Promise((resolve) => setTimeout(resolve, 2000));
+
+async function fetchProducts({ page }: { page: number }) {
   const response = await fetch(
     `https://bakery-api.fly.dev/products?page=${page}`
   );
-  const products = (await response.json()) as Product[];
-  return products;
+  await sleep();
+  const products = await response.json();
+  return products as Product[];
 }
 
-export function useProductListQuery({ page }) {
+export function useProductListQuery({ page }: { page: number }) {
   return useQuery({
-    queryKey: ["products"],
+    queryKey: ["products", { page }],
     queryFn: () => fetchProducts({ page }),
+    placeholderData: keepPreviousData,
+    staleTime: 20 * 1000,
+    gcTime: 5 * 1000,
   });
 }
